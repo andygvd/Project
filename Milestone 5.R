fbb1f8131137a6fc92d6d06e7d1d6229beb3a109
Andy Kan <72168163+andygvd@users.noreply.github.com>
2024-12-09 19:20:08 -0500

    Create Milestone 5.R
    Values for the Columns Age and Calories Burned
    I chose this because for the median age, it's also linked to the average calories burned

---
+++ Milestone 5.R
@@ -0,0 +1,27 @@
+> age <- gym$Age
+> mean_value <- mean(gym$Age)
+> median_value <- median(gym$Age)
+> variance_value <- var(gym$Age)
+> sd_value <- sd(gym$Age)
+> mean_value
+[1] 38.68345
+> median_value
+[1] 40
+> variance_value
+[1] 148.375
+> sd_value
+[1] 12.18093
+
+> calories <- gym$Calories_Burned
+> mean_value <- mean(gym$Calories_Burned)
+> median_value <- median(gym$Calories_Burned)
+> variance_value <- var(gym$Calories_Burned)
+> sd_value <- sd(gym$Calories_Burned)
+> mean_value
+[1] 905.4224
+> median_value
+[1] 893
+> variance_value
+[1] 74333.4
+> sd_value
+[1] 272.6415
