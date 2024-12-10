0430dd21fcb753067790704801a8abdb7cd4e566
Andy Kan <72168163+andygvd@users.noreply.github.com>
2024-12-09 19:53:43 -0500

    Create Milestone 6.R

---
+++ Milestone 6.R
@@ -0,0 +1,10 @@
+> plot(gym$`Weight (kg)`, gym$Calories_Burned,
++      main = "Scatterplot of Weight vs Calories Burned",
++      xlab = "Weight",
++      ylab = "Calories Burned",
++      pch = 16,
++      col = "blue")
+>      
+> correlation <- cor(gym$`Weight (kg)`, gym$Calories_Burned)
+> print(paste("Correlation: ", correlation))
+[1] "Correlation:  0.0954434730121706"
